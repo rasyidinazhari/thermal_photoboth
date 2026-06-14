8f0621de006adc1a34df8da169371c76f662b90f
rasyidinazhari <wisnurasyidinazhari99@gmail.com>
2026-06-14 18:24:10 +0700

    vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite"; // <-- Tambahkan ini
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
-})
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    nitro(), // <-- Tambahkan ini
+    viteReact(),
+  ],
+});
 
-export default config
+export default config;
